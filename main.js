257244b8b60578a9526a47fd561b5616d525a385
tekayafiras <63792357+tekayafiras@users.noreply.github.com>
2021-03-16 23:41:37 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const check = document.querySelector('.check');
 const ball = document.querySelector('.ball');
 const gridItems1 = document.querySelectorAll('.grid-items');
 const gridItems2 = document.querySelectorAll('.grid-items-2');
-const body = document.querySelector('body');
+const body = document.querySelector('.body');
 const p = document.querySelectorAll('p');
 const h1 = document.querySelectorAll('h1');
 const h2 = document.querySelectorAll('h2');
 const lighth1 = document.querySelector('footer h2');
+const bgPattern = document.querySelector('.bg');
 
 
 
@@ -14,7 +15,7 @@ check.addEventListener('click',function(e){
     ball.classList.toggle('move');
     ball.classList.toggle('ball-light');
     check.classList.toggle('toggle-light');
-    console.log(e.target)
+    
 
     gridItems1.forEach(function(item){
         item.classList.toggle('cardBG');
@@ -30,7 +31,6 @@ check.addEventListener('click',function(e){
         h.classList.toggle('titleText')
     })
     lighth1.classList.toggle('pText')
-    // if(ball.classList.contains('move')){
-
-    // }
+    bgPattern.classList.toggle('patternBg')
+    
 })
